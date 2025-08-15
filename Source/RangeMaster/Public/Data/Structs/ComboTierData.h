b835ccb7f5ff3c5b8b18fadea650c632c7c72533
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2025-08-15 14:37:24 +0300

    Add Combo Tier Data

---
+++ Source/RangeMaster/Public/Data/Structs/ComboTierData.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "ComboTierData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FComboTierData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combo Tier", meta = (ClampMin = "1"))
+	int32 Threshold = 10;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combo Tier", meta = (ClampMin = "1"))
+	int32 Multiplier = 1;
+};
