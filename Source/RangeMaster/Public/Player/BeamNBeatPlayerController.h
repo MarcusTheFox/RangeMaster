303dfbe3aa8115f665194aab37bb9c97288bfb86
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2025-08-15 18:36:11 +0300

    Begin migrate player controller to c++

---
+++ Source/RangeMaster/Public/Player/BeamNBeatPlayerController.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BeamNBeatPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RANGEMASTER_API ABeamNBeatPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
