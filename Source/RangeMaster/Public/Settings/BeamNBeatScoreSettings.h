159f542fe8c2c2066519fa15c15209d32a0ff7c2
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2025-08-15 14:37:50 +0300

    Add Score Settings

---
+++ Source/RangeMaster/Public/Settings/BeamNBeatScoreSettings.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Data/Structs/ComboTierData.h"
+#include "Engine/DeveloperSettings.h"
+#include "BeamNBeatScoreSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Config = Game, DefaultConfig, meta = (DisplayName = "Beam & Beat Settings - Score"))
+class RANGEMASTER_API UBeamNBeatScoreSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(Config, EditAnywhere, Category = "Score")
+	TArray<FComboTierData> ComboTiers;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Score")
+	int32 BasePoints;
+
+	static const UBeamNBeatScoreSettings* Get()
+	{
+		return GetDefault<UBeamNBeatScoreSettings>();
+	}
+};
