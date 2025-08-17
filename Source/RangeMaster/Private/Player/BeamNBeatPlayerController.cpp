30c3cf4d117dd05dad5d120d1af98825a5d72fc7
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2025-08-17 13:11:27 +0300

    Finish migrate player controller to C++

--- Source/RangeMaster/Private/Player/BeamNBeatPlayerController.cpp
+++ Source/RangeMaster/Private/Player/BeamNBeatPlayerController.cpp
@@ -2,3 +2,146 @@
 
 
 #include "Player/BeamNBeatPlayerController.h"
+
+#include "Core/RangeMasterGameMode.h"
+#include "FunctionLibraries/GameUtilityFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
+
+void ABeamNBeatPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (Subsystem)
+	{
+		Subsystem->AddMappingContext(DefaultMappingContext, 0);
+	}
+
+	SetInputMode_UI();
+
+	if (IsLocalController() && HUDContainerClass)
+	{
+		HUDContainerInstance = CreateWidget<UUserWidget>(this, HUDContainerClass);
+		if (HUDContainerInstance)
+		{
+			HUDContainerInstance->AddToViewport();
+		}
+	}
+
+	ARangeMasterGameMode* GameMode = UGameUtilityFunctionLibrary::GetRangeMasterGameMode(this);
+
+	if (!GameMode) return;
+
+	GameMode->OnPreparePhaseStarted.AddDynamic(this, &ABeamNBeatPlayerController::OnPreparePhaseStarted);
+	GameMode->OnGameFinished.AddDynamic(this, &ABeamNBeatPlayerController::OnGameFinished);
+	GameMode->OnGameStopped.AddDynamic(this, &ABeamNBeatPlayerController::OnGameStopped);
+	GameMode->OnGameReset.AddDynamic(this, &ABeamNBeatPlayerController::OnGameReset);
+}
+
+void ABeamNBeatPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		EnhancedInputComponent->BindAction(PauseAction, ETriggerEvent::Started, this,
+		                                   &ABeamNBeatPlayerController::OnPauseAction);
+	}
+}
+
+void ABeamNBeatPlayerController::GameResume()
+{
+	OnGameResumed.Broadcast();
+	UGameplayStatics::SetGamePaused(this, false);
+	SetInputMode_Game();
+}
+
+void ABeamNBeatPlayerController::GamePause()
+{
+	OnGamePaused.Broadcast();
+	UGameplayStatics::SetGamePaused(this, true);
+	SetInputMode_GameAndUI();
+}
+
+void ABeamNBeatPlayerController::OnPreparePhaseStarted()
+{
+	SetInputMode_Game();
+	AddInputMappingContext(PauseMappingContext);
+}
+
+void ABeamNBeatPlayerController::OnGameFinished(const FGameResultData& Result)
+{
+	SetInputMode_UI();
+	CenterCursor();
+	RemoveInputMappingContext(PauseMappingContext);
+}
+
+void ABeamNBeatPlayerController::OnGameStopped()
+{
+	SetInputMode_UI();
+	RemoveInputMappingContext(PauseMappingContext);
+}
+
+void ABeamNBeatPlayerController::OnGameReset()
+{
+	SetInputMode_Game();
+	UGameplayStatics::SetGamePaused(this, false);
+}
+
+void ABeamNBeatPlayerController::OnPauseAction(const FInputActionValue& Value)
+{
+	if (UGameplayStatics::IsGamePaused(this))
+	{
+		GameResume();
+	}
+	else
+	{
+		GamePause();
+	}
+}
+
+void ABeamNBeatPlayerController::SetInputMode_Game()
+{
+	FInputModeGameOnly InputMode;
+	SetInputMode(InputMode);
+	
+	bShowMouseCursor = false;
+}
+
+void ABeamNBeatPlayerController::SetInputMode_UI()
+{
+	FInputModeUIOnly InputMode;
+	SetInputMode(InputMode);
+	
+	bShowMouseCursor = true;
+}
+
+void ABeamNBeatPlayerController::SetInputMode_GameAndUI()
+{
+	FInputModeGameAndUI InputMode;
+	SetInputMode(InputMode);
+	
+	bShowMouseCursor = true;
+	CenterCursor();
+}
+
+void ABeamNBeatPlayerController::AddInputMappingContext(UInputMappingContext* MappingContext)
+{
+	Subsystem->AddMappingContext(MappingContext, 0);
+}
+
+void ABeamNBeatPlayerController::RemoveInputMappingContext(UInputMappingContext* MappingContext)
+{
+	Subsystem->RemoveMappingContext(MappingContext);
+}
+
+void ABeamNBeatPlayerController::CenterCursor()
+{
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+
+	const int32 CenterX = ViewportSizeX / 2;
+	const int32 CenterY = ViewportSizeY / 2;
+	
+	SetMouseLocation(CenterX, CenterY);
+}
--- Source/RangeMaster/Public/Player/BeamNBeatPlayerController.h
+++ Source/RangeMaster/Public/Player/BeamNBeatPlayerController.h
@@ -3,8 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
 #include "GameFramework/PlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Data/Structs/GameResultData.h"
 #include "BeamNBeatPlayerController.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnGameResumed);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnGamePaused);
 
 /**
  * 
@@ -13,4 +20,72 @@ UCLASS()
 class RANGEMASTER_API ABeamNBeatPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+	UFUNCTION(BlueprintCallable)
+	void GameResume();
+
+	UFUNCTION(BlueprintCallable)
+	void GamePause();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	
+	UFUNCTION()
+	void OnPreparePhaseStarted();
+	
+	UFUNCTION()
+	void OnGameFinished(const FGameResultData& Result);
+	
+	UFUNCTION()
+	void OnGameStopped();
+	
+	UFUNCTION()
+	void OnGameReset();
+
+	UFUNCTION()
+	void OnPauseAction(const FInputActionValue& Value);
+
+	UFUNCTION(BlueprintCallable, Category = "Input")
+	void SetInputMode_Game();
+	
+	UFUNCTION(BlueprintCallable, Category = "Input")
+	void SetInputMode_UI();
+	
+	UFUNCTION(BlueprintCallable, Category = "Input")
+	void SetInputMode_GameAndUI();
+
+	UFUNCTION(BlueprintCallable, Category = "Input")
+	void AddInputMappingContext(UInputMappingContext* MappingContext);
+
+	UFUNCTION(BlueprintCallable, Category = "Input")
+	void RemoveInputMappingContext(UInputMappingContext* MappingContext);
+	
+	UFUNCTION(BlueprintCallable, Category = "UI|Cursor")
+	void CenterCursor();
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "Game")
+	FOnGameResumed OnGameResumed;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "Game")
+	FOnGamePaused OnGamePaused;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI")
+	TSubclassOf<UUserWidget> HUDContainerClass;
+	
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "UI")
+	TObjectPtr<UUserWidget> HUDContainerInstance;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputMappingContext> PauseMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> PauseAction;
+
+private:
+	UPROPERTY()
+	TObjectPtr<UEnhancedInputLocalPlayerSubsystem> Subsystem;
 };
